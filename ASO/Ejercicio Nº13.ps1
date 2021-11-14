5576eb99f50ab19a90d0c98d663b6f30c2b07668
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-14 19:31:36 +0100

    Delete Ejercicio Nº13.ps1

--- ASO/Ejercicio Nº13.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-$Archivos_ejecutables_com=Get-ChildItem -Path C:\windows\System32 -Recurse -Include *.com | Select-Object Name,Length,Mode
-$Archivos_ejecutables_com
-$Archivos_ejecutables_exe=Get-ChildItem -Path C:\windows\System32 -Recurse -Include *.exe | Select-Object Name,Length,Mode
-$Archivos_ejecutables_exe
-
-$Archivos_ejecutables_com | Out-File -FilePath fichero.txt -NoNewline
-$Archivos_ejecutables_exe | Add-Content -Path fichero.txt -Newline
