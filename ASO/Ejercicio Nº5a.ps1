f4f128f547c638993a2842d3cb0904ba28b12a29
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-23 17:02:15 +0100

    Update Ejercicio Nº5a.ps1

--- ASO/Ejercicio Nº5a.ps1
+++ ASO/Ejercicio Nº5a.ps1
@@ -1,10 +1,12 @@
-﻿# Sin Argumentos
+﻿
+ #En esta parte le digo al usuario que me de dos numeros #
 echo ""
 $N1=Read-Host ("Introduzca un numero")
 echo ""
 $N2=Read-Host ("Introduzca un numero")
 echo ""
 
+#En la siguiente parte, me indica si los numeros son iguales o más grande que otro con las operaciones de comparación de Powershell#
 if($N1 -eq $N2)
 {
 Write-Host "Los numeros son iguales"
@@ -12,12 +14,12 @@ Write-Host "Los numeros son iguales"
 
 if($N1 -gt $N2)
 {
-Write-Host "El numero $N1 es mayor que $N2"
+Write-Host "El numero mayor es el $N1 "
 }
 
 if($N1 -lt $N2)
 {
-Write-Host "El numero $N2 es mayor que $N1"
+Write-Host "El numero mayor es el $N2"
 }
 
 
