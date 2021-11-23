9ec0281e75528e17648dc96112b925d20b1cfaf5
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-23 18:23:23 +0100

    Update Ejercicio Nº6.ps1

--- ASO/Ejercicio Nº6.ps1
+++ ASO/Ejercicio Nº6.ps1
@@ -1,7 +1,10 @@
+#Creo una función donde me calcula la factotial del numero que pusiste como argumento#
 function Get-Factorial ($n) {
+#Si pones 0 te saldra como resultado 1#
     if ($args[0] -eq 0) {
         return 1
     }
+    #Esta parte calcula el factorial#
     $fact = 1
     1..$n | ForEach { $fact *= $_ }
     return $fact
