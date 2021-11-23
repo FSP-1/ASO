ec02bf4d1d5f429fb14cc0eb44861aa811121a03
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-23 16:54:30 +0100

    Update Ejercicio Nº4.ps1

--- ASO/Ejercicio Nº4.ps1
+++ ASO/Ejercicio Nº4.ps1
@@ -1,5 +1,6 @@
 ﻿
-
+#Con la siguiente variable, suma los tres argumentos que hayas puesto al lado del script a la hora de ejecutarlo#
+#Para que funcione, hay que indicarle a powershell que los argumentos son Numericos con el [int]#
 $res=[int]$args[0]+ $args[1]+ $args[2]
 echo ""
 
