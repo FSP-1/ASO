702ccc40867b76e8e2e5b3ff63c50ebcfac98b18
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-23 18:45:53 +0100

    Update Ejercicio Nº11.ps1

--- ASO/Ejercicio Nº11.ps1
+++ ASO/Ejercicio Nº11.ps1
@@ -1,2 +1,4 @@
-﻿$Lista_variable=Get-Variable
+#Con el comando Get-variable me muestra las variables y su contenido#
+$Lista_variable=Get-Variable
+#Lo deridigo a un fichero con >>#
 $Lista_variable >> Lista_variable.txt
