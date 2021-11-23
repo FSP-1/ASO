8600f0b69ee74661f9080bc66512a842e7477319
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-23 18:31:46 +0100

    Update Ejercicio Nº7.ps1

--- ASO/Ejercicio Nº7.ps1
+++ ASO/Ejercicio Nº7.ps1
@@ -1,7 +1,10 @@
-﻿echo ""
+#Le pregunto al usuario un numero#
+ echo ""
 $numero=Read-Host ("Introduzca un numero ")
-$multiplicar=0
+$multiplicar=1
 echo ""
+#Creo un blucle donde me multiplica del 1 hasta 10 por la variable $numero#
+#El scrip pondra varias veces el resultado hasta que la variable $multi llege al 10#
 do
 {
 $Resultado=[int]$numero*$multiplicar
