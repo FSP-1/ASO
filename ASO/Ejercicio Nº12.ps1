114e0da5fd56e181d40835b8e27551a2d790e40f
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-23 18:49:02 +0100

    Update Ejercicio Nº12.ps1

--- ASO/Ejercicio Nº12.ps1
+++ ASO/Ejercicio Nº12.ps1
@@ -1,2 +1,4 @@
-﻿$Ficheros_txt=Get-ChildItem -Path C:\ -Recurse -Include *.txt
+#Con el comando get-childitem me deja ver tanto los directorios principales como sus subdirectorios#
+#Con el -Path le indico donde buscar y con -Recurse -Include que tipo de fichero quiero#
+$Ficheros_txt=Get-ChildItem -Path C:\ -Recurse -Include *.txt
 $Ficheros_txt >> Fichero_con_txt.txt
