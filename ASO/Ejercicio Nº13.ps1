6fe018bd66fb83f3d69eb4327b2151099f08678d
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-23 18:52:34 +0100

    Update Ejercicio Nº13.ps1

--- ASO/Ejercicio Nº13.ps1
+++ ASO/Ejercicio Nº13.ps1
@@ -1,7 +1,11 @@
-﻿##
+#En este script he separado la lista .com y la lista .exe para asi juntarlos en un fichero#
+
+#Me busca en el directorio  C:\windows\System32 los ficheros que tenga .com como extención #
 $Archivos_ejecutables_com=Get-ChildItem -Path C:\windows\System32 -Recurse -Include *.com 
-##
+
+#Me busca en el directorio  C:\windows\System32 los ficheros que tenga .exe como extención #
 $Archivos_ejecutables_exe=Get-ChildItem -Path C:\windows\System32 -Recurse -Include *.exe 
+#Meto las listas a un fichero#
 echo "________________.COM_______________________" >> Archivos_ejecutable.txt
 $Archivos_ejecutables_com >> Archivos_ejecutable.txt
 
