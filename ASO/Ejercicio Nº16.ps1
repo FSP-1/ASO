e59061f306fa7e0f6d4bab90b10a146b6ffff643
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-25 15:32:54 +0100

    Update Ejercicio Nº16.ps1

--- ASO/Ejercicio Nº16.ps1
+++ ASO/Ejercicio Nº16.ps1
@@ -1,4 +1,5 @@
-﻿
-$b=Get-Process | Sort-Object CPU -Descending | Select-Object CPU, Id ,ProcessName | Export-csv Proccess.csv -NoTypeInformation
-Import-Csv Proccess.csv
-
+#Obtengo los processos activos, los ordeno por el CPU con una tuberia y el comando Select-Object#
+#Lo Exporto en el fichero csv creado anteriormente#
+#Finalmente lo importo a csv para que se entienda mejor el contenido#
+Get-Process | Sort-Object CPU -Descending |Select-Object CPU,Id,ProcessName|  Export-Csv Proccess.csv -NoTypeInformation
+Import-Csv Proccess.csv >> Proccess.txt
