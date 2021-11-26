b239340aaa3bcf5315918873a4f3970870f36e62
Franco <77828917+FSP-1@users.noreply.github.com>
2021-11-26 10:04:30 +0100

    Delete ASO directory

--- ASO/Ejercicio Nº11.ps1
+++
@@ -1,4 +0,0 @@
-#Con el comando Get-variable me muestra las variables y su contenido#
-$Lista_variable=Get-Variable
-#Lo deridigo a un fichero con >>#
-$Lista_variable >> Lista_variable.txt
--- ASO/Ejercicio Nº12.ps1
+++
@@ -1,4 +0,0 @@
-#Con el comando get-childitem me deja ver tanto los directorios principales como sus subdirectorios#
-#Con el -Path le indico donde buscar y con -Recurse -Include que tipo de fichero quiero#
-$Ficheros_txt=Get-ChildItem -Path C:\ -Recurse -Include *.txt
-$Ficheros_txt >> Fichero_con_txt.txt
