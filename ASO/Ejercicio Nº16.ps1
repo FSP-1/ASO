7b1e8b1f12a5d7af58e13a9f9d0389e86ee6662c
Franco <77828917+FSP-1@users.noreply.github.com>
2022-01-25 11:34:38 +0100

    Delete ASO directory

--- ASO/Ejercicio Nº16.ps1
+++
@@ -1,5 +0,0 @@
-#Obtengo los processos activos, los ordeno por el CPU con una tuberia y el comando Select-Object#
-#Lo Exporto en el fichero csv creado anteriormente#
-#Finalmente lo importo a txt para que se entienda mejor el contenido#
-Get-Process | Sort-Object CPU -Descending |Select-Object CPU,Id,ProcessName|  Export-Csv Proccess.csv -NoTypeInformation
-Import-Csv Proccess.csv >> Proccess.txt
--- ASO/Ejercicio Nº9.ps1
+++
@@ -1,4 +0,0 @@
-#La primera parte me saca la lista del los -verb de Set y solo el tipo de comando y nombre#
-$Lista_Set=get-command -verb set | Select-Object CommandType, Name 
-#La segunda parte me lo pone en un fichero#
-$Lista_Set | Out-File Set.txt
